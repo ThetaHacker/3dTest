254b6b111498465e2ee6358c3baa511206002038
ThetaHacker <42395099+ThetaHacker@users.noreply.github.com>
2018-08-20 10:41:37 +1000

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,1 +1,13 @@
+// Variables
+var screen = {w:window.innerWidth, h:window.innerHeight};
+var c = document.getElementById("screen");
 
+// Resize Canvas
+c.width = screen.w
+c.height = screen.h
+
+// Setup
+var ctx = c.getContext("2d");
+
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", screen.w/2, c.height = screen.h/2);
