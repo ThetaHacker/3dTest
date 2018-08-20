2ad4b8610e279ee1ab0a4067397a92cc74bd65a5
ThetaHacker <42395099+ThetaHacker@users.noreply.github.com>
2018-08-20 12:44:18 +1000

    Update engine.js

--- engine.js
+++ engine.js
@@ -8,6 +8,7 @@
 // Variables
 var screen = {w:window.innerWidth, h:window.innerHeight};
 var c = document.getElementById("screen");
+var time = 0;
 
 // Resize Canvas
 c.width = screen.w
@@ -16,6 +17,15 @@ c.height = screen.h
 // Setup
 var ctx = c.getContext("2d");
 
-ctx.textAlign="center";
-ctx.font = "60px serif";
-ctx.fillText("3D Experiment", screen.w/2, screen.h/2);
+// Main loop
+
+function step() { 
+  ctx.clearRect(0,0,c.width,c.height)
+  ctx.textAlign="center";
+  ctx.font = "60px serif";
+  ctx.fillText("3D Experiment", screen.w/2, screen.h/2 + Math.sin(time)*30);
+  window.requestAnimationFrame(step);
+  time += 1;
+}
+
+window.requestAnimationFrame(step);
